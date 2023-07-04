66930286082ac5ddeee7a391d85c774142887e08
afik.cohen <aphexsa@gmail.com>
2023-07-04 16:28:33 -0700

    Revert "Added scroll functionality to keep the chat window fixed to the bottom. WIP"
    This reverts commit 51f066baac79827c9f2117c6bbd9c48bd5c1bd91.

--- editors/rift-vscode-svelte/webviews/components/Main.svelte
+++ editors/rift-vscode-svelte/webviews/components/Main.svelte
@@ -1,81 +1,62 @@
 <!-- Navbar.svelte -->
 <script lang="ts">
-    import {onMount} from "svelte";
-    import CopySvg from "./icons/CopySvg.svelte";
-    import UserInput from "./chat/UserInput.svelte";
-    import Response from "./chat/Response.svelte";
-    import Logs from "./logs/Logs.svelte";
-    import {loading, state} from "./stores";
-    import type {ChatAgentProgress} from "../../src/types";
+  import { onMount } from "svelte";
+  import CopySvg from "./icons/CopySvg.svelte";
+  import UserInput from "./chat/UserInput.svelte";
+  import Response from "./chat/Response.svelte";
+  import Logs from "./logs/Logs.svelte";
+  import { loading, state } from "./stores";
+  import type { ChatAgentProgress } from "../../src/types";
 
-    state.subscribe((state) => {
-        // state.history.splice(0);
+  state.subscribe((state) => {
+    // state.history.splice(0);
 
-        if (!state.history.length) return; // don't want initial rendering to fuck this up
-        vscode.setState(state);
-    });
+    if (!state.history.length) return; // don't want initial rendering to fuck this up
+    vscode.setState(state);
+  });
 
-    let progress: ChatAgentProgress;
-    let progressResponse = "";
-    let isDone = false;
-    const vscodeState = vscode.getState();
-    console.log("attempting to access vscode state:");
-    console.log(vscodeState);
-    if (vscodeState && vscodeState.history.length) state.set(vscodeState);
+  let progress: ChatAgentProgress;
+  let progressResponse = "";
+  let isDone = false;
+  const vscodeState = vscode.getState();
+  console.log("attempting to access vscode state:");
+  console.log(vscodeState);
+  if (vscodeState && vscodeState.history.length) state.set(vscodeState);
 
-
-    let scrollHeight = 0;
-    let fixedToBottom = true;
-
-    const observer = new MutationObserver(mutations => {
-        if (scrollHeight < document.documentElement.scrollHeight && fixedToBottom) {
-            window.scrollTo(0, document.documentElement.scrollHeight);
-        }
-        scrollHeight = document.documentElement.scrollHeight;
-    });
-
-    const observerConfig = {attributes: true, childList: true, subtree: true};
-
-    function handleScroll() {
-        fixedToBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 3;
+  const incomingMessage = (event) => {
+    console.log(event);
+    progress = event.data.data as ChatAgentProgress;
+    progressResponse = progress.response;
+    console.log(progressResponse);
+    isDone = progress.done;
+    if (isDone) {
+    state.update((state) => ({
+      ...state, history: [...state.history, { role: "assistant", content: progressResponse }]
+    }));
     }
+  };
 
-    window.addEventListener('scroll', handleScroll);
-
-    const incomingMessage = (event) => {
-        console.log(event);
-        progress = event.data.data as ChatAgentProgress;
-        progressResponse = progress.response;
-        console.log(progressResponse);
-        isDone = progress.done;
-        if (isDone) {
-            state.update((state) => ({
-                ...state, history: [...state.history, {role: "assistant", content: progressResponse}]
-            }));
-            observer.observe(document.documentElement, observerConfig);
-        }
-    }
-
+  
 </script>
 
-<svelte:window on:message={incomingMessage}/>
+<svelte:window on:message={incomingMessage} />
 
 <div>
-    <div style="height: 70vh;" class="flex flex-col overflow-y-auto">
-        {#each $state.history as item}
-            {#if item.role == "user"}
-                <UserInput value={item.content}/>
-            {:else}
-                <Response value={item.content}/>
-            {/if}
-        {/each}
-        {#if !isDone}
-            <Response value={progressResponse}/>
-        {/if}
-        <UserInput value={""} enabled={true}/>
-    </div>
-    <div style="height: 30vh;">
-        <!-- LOGS HERE -->
-        <Logs chatDone={isDone}/>
-    </div>
+  <div style="height: 70vh;" class="flex flex-col overflow-y-auto">
+    {#each $state.history as item}
+      {#if item.role == "user"}
+        <UserInput value={item.content}/>
+      {:else}
+        <Response value={item.content} />
+      {/if}
+    {/each}
+    {#if !isDone}
+      <Response value={progressResponse} />
+    {/if}
+    <UserInput value={""} enabled={true} />
+  </div>
+  <div style="height: 30vh;">
+    <!-- LOGS HERE -->
+    <Logs chatDone={isDone} />
+  </div>
 </div>
