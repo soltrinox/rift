1baad335b1b7c66f97ce443a8b5f6262e8d52510
afik.cohen <aphexsa@gmail.com>
2023-07-04 18:29:07 -0700

    Refactor UserInput.svelte to use double quotes for imports and add semicolons at the end of statements, and update the button and div elements to use consistent indentation.

--- editors/rift-vscode-svelte/webviews/components/chat/UserInput.svelte
+++ editors/rift-vscode-svelte/webviews/components/chat/UserInput.svelte
@@ -1,49 +1,54 @@
 <script lang="ts">
-  import SendSvg from '../icons/SendSvg.svelte';
-import { loading, state } from '../stores'
-  export let value: string = ''
-  export let enabled: boolean = false
+  import SendSvg from "../icons/SendSvg.svelte";
+  import { loading, state } from "../stores";
+  export let value: string = "";
+  export let enabled: boolean = false;
 
   function resize(event: Event) {
-    let targetElement = event.target as HTMLElement
-    targetElement.style.height = 'auto'
-    targetElement.style.height = `${targetElement.scrollHeight}px`
+    let targetElement = event.target as HTMLElement;
+    targetElement.style.height = "auto";
+    targetElement.style.height = `${targetElement.scrollHeight}px`;
   }
 
-  let textarea: HTMLTextAreaElement
+  let textarea: HTMLTextAreaElement;
 
   function sendMessage() {
-    textarea.blur()
-    loading.set(true)
+    textarea.blur();
+    loading.set(true);
 
     vscode.postMessage({
-      type: 'chatMessage',
-      messages: $state.history, 
+      type: "chatMessage",
+      messages: $state.history,
       message: textarea.value,
-    })
-    console.log('updating state...')
-    state.update((state) => ({ ...state, history: [...state.history, { role: 'user', content: textarea.value }] }))
-    textarea.value = ""
-    textarea.focus()
+    });
+    console.log("updating state...");
+    state.update((state) => ({
+      ...state,
+      history: [...state.history, { role: "user", content: textarea.value }],
+    }));
+    textarea.value = "";
+    textarea.focus();
   }
 
   function handleKeyDown(e: KeyboardEvent) {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       // 13 is the Enter key code
-      e.preventDefault() // Prevent default Enter key action
+      e.preventDefault(); // Prevent default Enter key action
       if (e.shiftKey) {
-        textarea.value = textarea.value + '\n'
-        textarea.style.height = 'auto'
-        textarea.style.height = textarea.scrollHeight + 'px'
-        return
+        textarea.value = textarea.value + "\n";
+        textarea.style.height = "auto";
+        textarea.style.height = textarea.scrollHeight + "px";
+        return;
       }
-      sendMessage()
+      sendMessage();
     }
     // logic to handle keydown event
   }
 </script>
 
-<div class="w-full text-md p-2 min-h-8 bg-[var(--vscode-input-background)] flex flex-row">
+<div
+  class="w-full text-md p-2 min-h-8 bg-[var(--vscode-input-background)] flex flex-row"
+>
   <textarea
     bind:this={textarea}
     class="w-full min-h-8 block outline-none focus:outline-none bg-transparent resize-none"
@@ -54,10 +59,10 @@ import { loading, state } from '../stores'
     {value}
   />
   {#if enabled}
-  <div class='justify-self-end flex-shrink-0'>
-  <button on:click={sendMessage} class="flex items-center flex-shrink">
-    <SendSvg />
-  </button>
-  </div> 
+    <div class="justify-self-end flex-shrink-0">
+      <button on:click={sendMessage} class="flex items-center flex-shrink">
+        <SendSvg />
+      </button>
+    </div>
   {/if}
 </div>
