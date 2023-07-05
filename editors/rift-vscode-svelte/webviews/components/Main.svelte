c879b109e2d858d0b7b38ecace53c5aaafe937a8
David Anderson <david.anderson@involio.com>
2023-07-04 18:15:57 -0700

    add divider bar

--- editors/rift-vscode-svelte/webviews/components/Main.svelte
+++ editors/rift-vscode-svelte/webviews/components/Main.svelte
@@ -1,13 +1,14 @@
 <!-- Navbar.svelte -->
 <script lang="ts">
-  import { onMount, onDestroy, tick} from "svelte";
+  import { onMount, onDestroy, tick } from "svelte";
   import CopySvg from "./icons/CopySvg.svelte";
+  import EllipsisSvg from "./icons/EllipsisSvg.svelte";
   import UserInput from "./chat/UserInput.svelte";
   import Response from "./chat/Response.svelte";
   import Logs from "./logs/Logs.svelte";
   import { loading, state } from "./stores";
   import type { ChatAgentProgress } from "../../src/types";
-  import Header from './Header.svelte'
+  import Header from "./Header.svelte";
   state.subscribe((state) => {
     // state.history.splice(0);
 
@@ -30,33 +31,40 @@
     // console.log(progressResponse);
     isDone = progress.done;
     // for sticky window
-    if(chatWindow.scrollHeight > height && fixedToBottom) {
-        chatWindow.scrollTo(0, chatWindow.scrollHeight)
+    if (chatWindow.scrollHeight > height && fixedToBottom) {
+      chatWindow.scrollTo(0, chatWindow.scrollHeight);
     }
-    height = chatWindow.scrollHeight
+    height = chatWindow.scrollHeight;
     // for sticky window^
     if (isDone) {
-    state.update((state) => ({
-      ...state, history: [...state.history, { role: "assistant", content: progressResponse }]
-    }));
+      state.update((state) => ({
+        ...state,
+        history: [
+          ...state.history,
+          { role: "assistant", content: progressResponse },
+        ],
+      }));
     }
   };
-  let chatWindow: HTMLDivElement
+  let chatWindow: HTMLDivElement;
   let fixedToBottom: boolean;
-  let height: number
+  let height: number;
   onMount(async () => {
     await tick();
     chatWindow.scrollTo(0, chatWindow.scrollHeight);
     height = chatWindow.scrollHeight;
-    fixedToBottom = chatWindow.clientHeight + chatWindow.scrollTop >= chatWindow.scrollHeight - 3;
-    chatWindow.addEventListener('scroll', function() {
-      if(!chatWindow.scrollTop || !chatWindow.scrollHeight) throw new Error();
-      console.log('scroll');
-      fixedToBottom = Boolean(chatWindow.clientHeight + chatWindow.scrollTop >= chatWindow.scrollHeight - 20);
+    fixedToBottom =
+      chatWindow.clientHeight + chatWindow.scrollTop >=
+      chatWindow.scrollHeight - 3;
+    chatWindow.addEventListener("scroll", function () {
+      if (!chatWindow.scrollTop || !chatWindow.scrollHeight) throw new Error();
+      console.log("scroll");
+      fixedToBottom = Boolean(
+        chatWindow.clientHeight + chatWindow.scrollTop >=
+          chatWindow.scrollHeight - 20
+      );
     });
   });
-
-  
 </script>
 
 <svelte:window on:message={incomingMessage} />
@@ -64,22 +72,30 @@
 <div>
   <Header />
   <div>
-  <div bind:this={chatWindow} class="flex flex-col overflow-y-auto max-h-[70vh]">
-    {#each $state.history as item}
-      {#if item.role == "user"}
-        <UserInput value={item.content}/>
-      {:else}
-        <Response value={item.content} />
+    <div
+      bind:this={chatWindow}
+      class="flex flex-col overflow-y-auto max-h-[70vh]"
+    >
+      {#each $state.history as item}
+        {#if item.role == "user"}
+          <UserInput value={item.content} />
+        {:else}
+          <Response value={item.content} />
+        {/if}
+      {/each}
+      {#if !isDone}
+        <Response value={progressResponse} />
       {/if}
-    {/each}
-    {#if !isDone}
-      <Response value={progressResponse} />
-    {/if}
-    <UserInput value={""} enabled={true} />
+      <UserInput value={""} enabled={true} />
+    </div>
+    <div class="max-h-[30vh]">
+      <section
+        id="divider"
+        class="border-t-2 pt-1 pb-2 hero container max-w-screen-lg mx-auto flex justify-center"
+      >
+        <EllipsisSvg />
+      </section>
+      <Logs chatDone={isDone} />
+    </div>
   </div>
-  <div>
-    <!-- LOGS HERE -->
-    <Logs chatDone={isDone} />
-  </div>
-</div>
 </div>
