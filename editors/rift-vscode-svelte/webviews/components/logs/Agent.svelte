673549c66e6d43117a39e04064cc76ce8ef440e4
David Anderson <david.anderson@involio.com>
2023-07-16 22:45:41 -0700

    delete agent working

--- editors/rift-vscode-svelte/webviews/components/logs/Agent.svelte
+++ editors/rift-vscode-svelte/webviews/components/logs/Agent.svelte
@@ -64,6 +64,20 @@
     const handleCancelAgent = (e: MouseEvent) => {
         vscode.postMessage({ type: "cancelAgent", id: id });
     };
+    const handleDeleteAgent = (e: MouseEvent) => {
+        //vscode.postMessage({ type: "cancelAgent", id: id });
+        state.update((state) => {
+            // Create a copy of the agents object without the specified agentId
+            const updatedAgents = { ...state.agents };
+            delete updatedAgents[id];
+
+            // Update the state with the new agents object
+            return {
+                ...state,
+                agents: updatedAgents,
+            };
+        });
+    };
 </script>
 
 <div class:bg-[var(--vscode-editor-hoverHighlightBackground)]={isSelected}>
@@ -135,7 +149,10 @@
                             >
                         </li>
                         <li class="list-item">
-                            <button class="btn px-2">Delete</button>
+                            <button
+                                class="btn px-2"
+                                on:click={handleDeleteAgent}>Delete</button
+                            >
                         </li>
                     </ul>
                 </div>
