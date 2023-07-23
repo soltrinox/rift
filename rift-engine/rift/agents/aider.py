4efb940084207ad3316a9f54c36f60c7d6dcd423
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-07-23 03:27:42 +0200

    Intercept commit.
    So now 2 kinds of events are intercepted:
    - on_write for every file change
    - on_commit when aider wants to commit after all the file changes.

--- rift-engine/rift/agents/aider.py
+++ rift-engine/rift/agents/aider.py
@@ -33,20 +33,35 @@ class AiderAgent(Agent):
 """
 
     async def run(self) -> AsyncIterable[List[file_diff.FileChange]]:
+        """
+        Example use:
+            python -m rift.agents.aider --port 7797 --debug False --args '["--model", "gpt-3.5-turbo", "rift/agents/aider.py"]'
+        """
         params = self.run_params
-
-        print(f"Number of args: {len(params.args)}")  # Added print statement
 
         logger.info(f"Aider: args: {params.args}")
 
         await ainput("\n> Press any key to continue.\n")
+
         logger.info("Running aider")
 
-        def on_write(filename: str, content: str):
-            logger.info(f"Writing {filename} with content {content}")
+        file_changes: List[file_diff.FileChange] = []
+
+        # This is called every time aider writes a file
+        # Instead of writing, this stores the file change in a list
+        def on_write(filename: str, new_content: str):
+            file_changes.append(file_diff.get_file_change(path=filename, new_content=new_content))
+            logger.info(f"Intercepted Write to {filename}")
+
+        # This is called when aider wants to commit after writing all the files
+        # This is where the user should accept/reject the changes
+        def on_commit():
+            logger.info("Intercepted Commit")
+
         from concurrent import futures
         with futures.ThreadPoolExecutor(1) as pool:
-            res = await asyncio.get_running_loop().run_in_executor(pool, aider.main, params.args, on_write)
+            res = await asyncio.get_running_loop().run_in_executor(
+                pool, aider.main, params.args, on_write, on_commit)
 
         await ainput("\n> Press any key to continue.\n")
 
