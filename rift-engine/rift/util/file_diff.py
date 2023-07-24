1d1a8ce2ef756dd2b173c920ff074f5c40b40a0b
jesse <jessemhan@proton.me>
2023-07-24 00:31:50 -0700

    wip
    wip

--- rift-engine/rift/util/file_diff.py
+++ rift-engine/rift/util/file_diff.py
@@ -45,9 +45,7 @@ def get_file_change(path: str, new_content: str) -> FileChange:
         return FileChange(uri=uri, old_content="", new_content=new_content, is_new_file=True)
 
 
-def edits_from_file_change(
-    file_change: FileChange, user_confirmation: bool = False
-) -> WorkspaceEdit:
+def edits_from_file_change(file_change: FileChange, user_confirmation: bool = False) -> WorkspaceEdit:
     dmp = diff_match_patch()
     diff = dmp.diff_lineMode(file_change.old_content, file_change.new_content, None)
     dmp.diff_cleanupSemantic(diff)
@@ -55,45 +53,56 @@ def edits_from_file_change(
     line = 0  # current line number
     char = 0  # current character position within the line
     edits = []  # list of TextEdit objects
+    annotation_label = file_change.annotation_label or "rift"
+
+    new_text = ""
 
     for op, text in diff:
-        # count the number of lines in `text` and the number of characters in the last line
-        lines = text.split("\n")
-        last_line_chars = len(lines[-1])
-        line_count = len(lines) - 1  # don't count the current line
+        if op == -1: # remove
+            pass
+        elif op == 0: # keep
+            new_text += text
+        elif op == 1: # add
+            new_text += text
 
-        end_line = line + line_count
-        end_char = (
-            char + last_line_chars if line_count == 0 else last_line_chars
-        )  # if we moved to a new line, start at char 0
+    # for op, text in diff:
+    #     # count the number of lines in `text` and the number of characters in the last line
+    #     lines = text.split("\n")
+    #     last_line_chars = len(lines[-1])
+    #     line_count = len(lines) - 1  # don't count the current line
 
-        if op == -1:
-            # text was deleted
-            edits.append(
-                TextEdit(Range.mk(line, char, end_line, end_char), "", annotationId="rift")
-            )
-        elif op == 1:
-            # text was added
-            edits.append(
-                TextEdit(Range.mk(line, char, line, char), text, annotationId="rift")
-            )  # new text starts at the current position
+    #     end_line = line + line_count
+    #     end_char = (
+    #         char + last_line_chars if line_count == 0 else last_line_chars
+    #     )  # if we moved to a new line, start at char 0
 
-        # update position
-        line = end_line
-        char = end_char
+    #     if op == -1:
+    #         # text was deleted
+    #         edits.append(TextEdit(Range.mk(line, char, end_line, end_char), "", annotationId=annotation_label))
+    #     elif op == 1:
+    #         # text was added
+    #         edits.append(
+    #             TextEdit(Range.mk(line, char, line, char), text, annotationId=annotation_label)
+    #         )  # new text starts at the current position
+    #     elif op == 0:
+    #         # text remains the same:
+            
+
+    #     # update position
+    #     line = end_line
+    #     char = end_char
+    lines = file_change.old_content.split("\n")
+    edits = [TextEdit(Range.mk(0,0,len(lines),len(lines[-1])), new_text, annotationId=annotation_label)]
 
     documentChanges = []
 
     changeAnnotations: dict[lsp.ChangeAnnotationIdentifier, lsp.ChangeAnnotation] = dict()
     if file_change.is_new_file:
-        documentChanges.append(
-            CreateFile(kind="create", uri=file_change.uri.uri, annotationId="rift")
-        )
+        documentChanges.append(CreateFile(kind="create", uri=file_change.uri.uri, annotationId=annotation_label))
     documentChanges.append(TextDocumentEdit(textDocument=file_change.uri, edits=edits))
-    changeAnnotations["rift"] = lsp.ChangeAnnotation(
-        label="rift", needsConfirmation=user_confirmation, description=None
-    )
+    changeAnnotations[annotation_label] = lsp.ChangeAnnotation(label=annotation_label, needsConfirmation=user_confirmation, description=None)
     return WorkspaceEdit(documentChanges=documentChanges, changeAnnotations=changeAnnotations)
+
 
 
 def edits_from_file_changes(
