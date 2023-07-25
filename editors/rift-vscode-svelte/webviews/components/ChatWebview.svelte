9d9749d688fbbefcedc89bd82e842a686432744f
brentably <brentably@gmail.com>
2023-07-24 18:47:34 -0700

    added ui component of accept reject bar. need to add business logic plus dynamic rendering

--- editors/rift-vscode-svelte/webviews/components/ChatWebview.svelte
+++ editors/rift-vscode-svelte/webviews/components/ChatWebview.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+  import AcceptRejectBar from "./AcceptRejectBar.svelte";
   import Chat from "./chat/Chat.svelte";
-  import OmniBar from "./chat/OmniBar.svelte";
+  import OmniBar from "./OmniBar.svelte";
   vscode.postMessage({type: "refreshState"})
 
 
@@ -8,5 +9,6 @@
 
 <div class="h-screen flex flex-col">
   <Chat />
+  <AcceptRejectBar/>
   <OmniBar />
 </div>
