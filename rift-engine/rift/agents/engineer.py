3864be768b4e393db65412b8b40c13e0841a9d30
jesse <jessemhan@proton.me>
2023-07-27 05:59:54 -0700

    wip

--- rift-engine/rift/agents/engineer.py
+++ rift-engine/rift/agents/engineer.py
@@ -1,3 +1,4 @@
+import time
 import asyncio
 import logging
 import uuid
@@ -26,6 +27,8 @@ from rift.llm.abstract import AbstractCodeCompletionProvider, InsertCodeResult
 from rift.lsp import LspServer as BaseLspServer
 from rift.lsp.document import TextDocumentItem
 from rift.server.selection import RangeSet
+from rift.util import file_diff
+from rift.util.TextStream import TextStream
 
 try:
     import gpt_engineer
@@ -35,11 +38,6 @@ try:
 except ImportError:
     raise Exception("`gpt_engineer` not found. Try `pip install gpt-engineer`")
 
-UPDATES_QUEUE = asyncio.Queue()
-INPUT_PROMPT_QUEUE = asyncio.Queue()
-INPUT_RESPONSE_QUEUE = asyncio.Queue()
-TASK_QUEUE = asyncio.Queue()
-OUTPUT_CHAT_QUEUE = asyncio.Queue()
 SEEN = set()
 
 import json
@@ -56,117 +54,58 @@ import rift.llm.openai_types as openai
 
 logger = logging.getLogger(__name__)
 
+def _fix_windows_path(path: str) -> str:
+    """
+    Replace a windows path represented as "/c%3A"... with "c:"...
 
-async def __popup_input(prompt: str) -> str:
-    await INPUT_PROMPT_QUEUE.put(prompt)
-    while True:
-        try:
-            resp = await asyncio.wait_for(INPUT_RESPONSE_QUEUE.get(), timeout=1.0)
-            break
-        except:
-            continue
-    return resp
+    :param path: Original path
+    :return: Usable windows path, or original path if not a windows path
+    """
+    pattern = r'^/(.)%3A'
 
+    match = re.match(pattern, path)
 
-def __popup_input_wrapper(prompt: str = "") -> str:
-    try:
-        loop = asyncio.get_running_loop()
-        result_future = loop.create_future()
+    if match:
+        drive_letter = match.group(1)
+        return path.replace(f"/{drive_letter}%3A", f"{drive_letter}:")
+    else:
+        return path
 
-        tsk = loop.create_task(__popup_input(prompt))
-        tsk.add_done_callback(lambda t: result_future.set_result(t.result()))
-
-        while not result_future.done():
-            loop.run_until_complete(asyncio.sleep(0.1))
-
-        return result_future.result()
-    except:
-        return asyncio.run(__popup_input(prompt))
-
-
-gpt_engineer.steps.input = __popup_input_wrapper
+# async def _popup_input(prompt: str) -> str:
+#     await INPUT_PROMPT_QUEUE.put(prompt)
+#     while True:
+#         try:
+#             resp = await asyncio.wait_for(INPUT_RESPONSE_QUEUE.get(), timeout=1.0)
+#             break
+#         except:
+#             continue
+#     return resp
 
 
-async def __popup_chat(prompt: str = "NONE", end=""):
-    await OUTPUT_CHAT_QUEUE.put(prompt)
+# def _popup_input_wrapper(prompt: str="") -> str:
+#     try:
+#         loop = asyncio.get_running_loop()
+#         result_future = loop.create_future()
 
+#         tsk = loop.create_task(_popup_input(prompt))
+#         tsk.add_done_callback(
+#             lambda t: result_future.set_result(t.result())
+#         )
 
-def __popup_chat_wrapper(prompt: str = "NONE", end=""):
-    try:
-        loop = asyncio.get_running_loop()
-        tsk = loop.create_task(__popup_chat(prompt, end))
-        tsk.add_done_callback(lambda t: print(f"Task completed with: {t.result()}"))
-    except:
-        asyncio.run(__popup_chat(prompt, end))
+#         while not result_future.done():
+#             loop.run_until_complete(asyncio.sleep(0.1))
 
+#         return result_future.result()
+#     except:
+#         return asyncio.run(_popup_input(prompt))
 
-gpt_engineer.ai.print = __popup_chat_wrapper
-gpt_engineer.steps.print = __popup_chat_wrapper
+# async def _popup_chat(prompt: str="NONE", end=""):
+#     # await OUTPUT_CHAT_QUEUE.put(prompt)
+#     response_stream.feed_data(prompt)
 
 from asyncio import Lock
 
 response_lock = Lock()
-
-
-async def _main(
-    project_path: str = "/Users/jwd2488/Documents/repos/rift/rift-engine/rift/agents/",
-    model: str = "gpt-4",
-    temperature: float = 0.1,
-    steps_config: StepsConfig = StepsConfig.DEFAULT,
-    verbose: bool = typer.Option(False, "--verbose", "-v"),
-    **kwargs,
-) -> DBs:
-    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
-    model = fallback_model(model)
-    ai = AI(
-        model=model,
-        temperature=temperature,
-    )
-
-    input_path = Path(project_path).absolute()
-    memory_path = input_path / "memory"
-    workspace_path = input_path / "workspace"
-    archive_path = input_path / "archive"
-
-    dbs = DBs(
-        memory=DB(memory_path),
-        logs=DB(memory_path / "logs"),
-        input=DB(input_path),
-        workspace=DB(workspace_path, in_memory_dict={}),  # in_memory_dict={}),
-        preprompts=DB(Path(gpt_engineer.__file__).parent / "preprompts"),
-        archive=DB(archive_path),
-    )
-
-    if steps_config not in [
-        StepsConfig.EXECUTE_ONLY,
-        StepsConfig.USE_FEEDBACK,
-        StepsConfig.EVALUATE,
-    ]:
-        archive(dbs)
-
-    steps = STEPS[steps_config]
-    from concurrent import futures
-
-    # await my_in.send_progress()
-
-    counter = 0
-    with futures.ThreadPoolExecutor(1) as pool:
-        for step in steps:
-            await asyncio.sleep(0.1)
-            messages = await asyncio.get_running_loop().run_in_executor(pool, step, ai, dbs)
-            await asyncio.sleep(0.1)
-            dbs.logs[step.__name__] = json.dumps(messages)
-            items = list(dbs.workspace.in_memory_dict.items())
-            if len([x for x in items if x[0] not in SEEN]) > 0:
-                await UPDATES_QUEUE.put([x for x in items if x[0] not in SEEN])
-                for x in items:
-                    if x[0] in SEEN:
-                        pass
-                    else:
-                        SEEN.add(x[0])
-            await asyncio.sleep(0.5)
-            counter += 1
-
 
 # dataclass for representing the result of the code completion agent run
 @dataclass
@@ -197,7 +136,7 @@ class EngineerAgentState(AgentState):
 
 # decorator for creating the code completion agent
 @agent(
-    agent_description="Generate code following an and ask clarifying questions.",
+    agent_description="Specify what you want it to build, the AI asks for clarification, and then builds it.",
     display_name="GPT Engineer",
 )
 @dataclass
@@ -205,12 +144,159 @@ class EngineerAgent(Agent):
     state: EngineerAgentState
     agent_type: ClassVar[str] = "engineer"
 
+    async def _main(
+        self,
+        prompt: Optional[str] = None,
+        project_path: str = "",
+        model: str = "gpt-4",
+        temperature: float = 0.1,
+        steps_config: StepsConfig = StepsConfig.DEFAULT,
+        verbose: bool = typer.Option(False, "--verbose", "-v"),
+        **kwargs,
+    ) -> DBs:
+
+        loop = asyncio.get_event_loop()        
+        def _popup_chat_wrapper(prompt: str="NONE", end=""):
+            def _worker():
+                self.response_stream.feed_data(prompt)
+            loop.call_soon_threadsafe(_worker)
+
+        def _popup_input_wrapper(prompt=""):
+            return "c"
+            # self.state.messages.append(openai.Message.assistant(prompt))
+            # t: asyncio.Task = loop.create_task(self.request_chat(RequestChatRequest(messages=self.state.messages)))
+            # while not t.done():
+            #     # loop.run_until_complete(asyncio.sleep(0.1))
+            #     logger.info("LOOPINg")
+            #     time.sleep(0.25)
+            # return t.result()
+        #     # result_future = loop.create_future()
+        #     # return asyncio.get_event_loop().call_soon_threadsafe()
+
+        gpt_engineer.ai.print = _popup_chat_wrapper
+        gpt_engineer.steps.print = _popup_chat_wrapper
+        gpt_engineer.steps.input = _popup_input_wrapper
+        self.response_stream = TextStream()
+
+        UPDATES_QUEUE = asyncio.Queue()
+        INPUT_PROMPT_QUEUE = asyncio.Queue()
+        INPUT_RESPONSE_QUEUE = asyncio.Queue()
+        TASK_QUEUE = asyncio.Queue()
+        OUTPUT_CHAT_QUEUE = asyncio.Queue()
+
+        STEPS_AGENT_TASKS_NAME_QUEUE = asyncio.Queue()
+        STEPS_AGENT_TASKS_EVENT_QUEUE = asyncio.Queue()    
+        logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+        model = fallback_model(model)
+        ai = AI(
+            model=model,
+            temperature=temperature,
+        )
+
+        input_path = _fix_windows_path(project_path)
+
+        gpteng_path = os.path.join(input_path, ".gpteng")
+
+        if not os.path.exists(gpteng_path):
+            os.makedirs(gpteng_path)
+
+        if prompt and not os.path.exists(os.path.join(input_path, "prompt")):
+            with open(os.path.join(input_path, "prompt"), "w") as f:
+                f.write(prompt)
+
+        memory_path = os.path.join(gpteng_path, "memory")
+        workspace_path = os.path.join(input_path) # pipe files directly into the workspace
+        archive_path = os.path.join(gpteng_path, "archive")
+
+        dbs = DBs(
+            memory=DB(memory_path),
+            logs=DB(os.path.join(memory_path, "logs")),
+            input=DB(workspace_path),
+            workspace=DB(workspace_path, in_memory_dict={}),  # in_memory_dict={}),
+            preprompts=DB(Path(gpt_engineer.__file__).parent / "preprompts"),
+            archive=DB(archive_path),
+        )
+
+        # if steps_config not in [
+        #     StepsConfig.EXECUTE_ONLY,
+        #     StepsConfig.USE_FEEDBACK,
+        #     StepsConfig.EVALUATE,
+        # ]:
+        #     archive(dbs)
+
+        steps = STEPS[steps_config]
+        from concurrent import futures
+
+            # Add all steps to task list
+        steps = STEPS[steps_config]
+        from concurrent import futures
+
+        step_events: Dict[int, asyncio.Event] = dict()
+        for i, step in enumerate(steps):
+            event = asyncio.Event()
+            step_events[i] = event
+            async def _step_task(event: asyncio.Event):
+                await event.wait()
+            _ = self.add_task(AgentTask(description=step.__name__, task=_step_task, args=[event]))    
+
+        # # Add all steps to task list
+        # for step in steps:
+        #     await STEPS_AGENT_TASKS_NAME_QUEUE.put(step._name_)
+
+        counter = 0
+        with futures.ThreadPoolExecutor(1) as pool:
+            for i, step in enumerate(steps):
+                await asyncio.sleep(0.1)
+                messages = await loop.run_in_executor(pool, step, ai, dbs)
+                await asyncio.sleep(0.1)
+                dbs.logs[step.__name__] = json.dumps(messages)
+                items = list(dbs.workspace.in_memory_dict.items())
+                updates = [x for x in items if x[0] not in SEEN]
+                if len(updates) > 0:
+                    # for file_path, new_contents in updates:
+                    await self.server.apply_workspace_edit(lsp.ApplyWorkspaceEditParams(file_diff.edits_from_file_changes([file_diff.get_file_change(
+                        file_path, new_contents
+                    ) for file_path, new_contents in updates], user_confirmation=True)))
+                    for x in items:
+                        if x[0] in SEEN:
+                            pass
+                        else:
+                            SEEN.add(x[0])
+
+                # Mark this step as complete
+                # # event = await STEPS_AGENT_TASKS_EVENT_QUEUE.get()
+                # event.set()
+                step_events[i].set()
+                await asyncio.sleep(0.5)
+                counter += 1               
+
+
+    async def _run_chat_thread(self):
+        print("Started handler thread")
+        response = ""
+        while True:
+            try:
+                async for delta in self.response_stream:
+                    if delta != "NONE":
+                        response += delta
+                        async with response_lock:
+                            await self.send_progress(EngineerProgress(response=response))
+                    else:
+                        await self.send_progress(EngineerProgress(response=response, done_streaming=True))
+
+                        async with response_lock:
+                            self.state.messages.append(openai.Message.assistant(content=response))
+            except AttributeError:
+                await asyncio.sleep(1)
+                continue
+
     @classmethod
     def create(cls, params: EngineerAgentParams, model, server):
         state = EngineerAgentState(
             model=model,
             params=params,
-            messages=[openai.Message.assistant("Hello! How can I help you today?")],
+            messages=[openai.Message.assistant("What do you want to build?")],
+
         )
         obj = cls(
             state=state,
@@ -218,74 +304,72 @@ class EngineerAgent(Agent):
             server=server,
         )
 
-        async def __run_chat_thread(obj):
-            print("Started handler thread")
+                
 
-            while True:
-                try:
-                    response = ""
-                    await obj.send_progress()
-                    toSend = await asyncio.wait_for(OUTPUT_CHAT_QUEUE.get(), timeout=1.0)
-                    for delta in toSend:
-                        response += delta
-                        async with response_lock:
-                            await obj.send_progress(EngineerProgress(response=response))
-                    await obj.send_progress(
-                        EngineerProgress(response=response, done_streaming=True)
-                    )
+        # async def _run_popup_thread(obj):
+        #     while True:
+        #         try:
+        #             prompt = await asyncio.wait_for(INPUT_PROMPT_QUEUE.get(), timeout=1.0)
+        #             if prompt != "":
+        #                 await asyncio.wait_for(OUTPUT_CHAT_QUEUE.put(prompt), timeout=1.0)
+        #             response = await obj.request_chat(RequestChatRequest(messages=obj.state.messages))
+        #             async with response_lock:
+        #                 obj.state.messages.append(openai.Message.user(content=response))
+        #             await INPUT_RESPONSE_QUEUE.put(response)
 
-                    async with response_lock:
-                        obj.state.messages.append(openai.Message.assistant(content=response))
-                except asyncio.TimeoutError:
-                    continue
+        #         except asyncio.TimeoutError:
+        #             continue
 
-        async def __run_popup_thread(obj):
-            while True:
-                try:
-                    await obj.send_progress()
-                    prompt = await asyncio.wait_for(INPUT_PROMPT_QUEUE.get(), timeout=1.0)
-                    if prompt != "":
-                        await asyncio.wait_for(OUTPUT_CHAT_QUEUE.put(prompt), timeout=1.0)
-                    response = await obj.request_chat(
-                        RequestChatRequest(messages=obj.state.messages)
-                    )
-                    async with response_lock:
-                        obj.state.messages.append(openai.Message.user(content=response))
-                    await INPUT_RESPONSE_QUEUE.put(response)
+        # async def _run_create_task_thread(obj: EngineerAgent):
+        #     while True:
+        #         try:
+        #             # Wait till we get the name of a new task to spawn
+        #             task_name = await asyncio.wait_for(STEPS_AGENT_TASKS_NAME_QUEUE.get(), timeout=1.0)
+        #             # Create an event to trigger when the task is complete
+        #             event = asyncio.Event()
+        #             # Put the event object on the queue so that it can be triggered
+        #             await STEPS_AGENT_TASKS_EVENT_QUEUE.put(event)
+        #             # Setup the function that will be waiting on the event
+        #             async def _event_wait():
+        #                 await event.wait()
+        #                 return True
 
-                except asyncio.TimeoutError:
-                    continue
+        #             _ = asyncio.get_running_loop().create_task(obj.add_task(AgentTask(task_name, _event_wait)).run())
 
-        asyncio.create_task(__run_chat_thread(obj))
-        asyncio.create_task(__run_popup_thread(obj))
+        #             await obj.send_progress()
+        #         except asyncio.TimeoutError:
+        #             continue
+
+        # asyncio.create_task(_run_create_task_thread(obj))
+        # asyncio.create_task(_run_chat_thread(obj))
+        # asyncio.create_task(_run_popup_thread(obj))
 
         return obj
 
+
     async def run(self) -> AgentRunResult:  # main entry point
         await self.send_progress()
-        steps = STEPS["default"]
-        from concurrent import futures
+        asyncio.create_task(self._run_chat_thread())
 
-        tasks = []
-        for step in steps:
-            tsk = AgentTask(step.__name__, None)
-            tasks.append(tsk)
-        self.set_tasks(tasks)
-        main_t = asyncio.create_task(_main())
+        async def get_prompt():
+            prompt = await self.request_chat(RequestChatRequest(messages=self.state.messages))
+            self.state.messages.append(openai.Message.user(prompt))
+            return prompt
 
-        counter = 0
-        while (not main_t.done()) or (UPDATES_QUEUE.qsize() > 0):
-            counter += 1
-            try:
-                updates = await asyncio.wait_for(UPDATES_QUEUE.get(), 1.0)
-                for file_path, new_contents in updates:
-                    await self.server.apply_workspace_edit(
-                        lsp.ApplyWorkspaceEditParams(
-                            file_diff.edits_from_file_change(
-                                file_diff.get_file_change(file_path, new_contents)
-                            )
-                        )
-                    )
+        get_prompt_task = self.add_task(AgentTask("Get prompt for workspace", get_prompt))
+        prompt = await get_prompt_task.run()
+        
+        await asyncio.create_task(self._main(prompt=prompt, project_path=self.state.params.workspaceFolderPath))
+        # counter = 0
+        # while (not main_t.done()) or (UPDATES_QUEUE.qsize() > 0):
+        #     counter += 1
+        #     try:
+        #         updates = await asyncio.wait_for(UPDATES_QUEUE.get(), 1.0)
+        #         for file_path, new_contents in updates:
+        #             await self.server.apply_workspace_edit(lsp.ApplyWorkspaceEditParams(file_diff.edits_from_file_change(file_diff.get_file_change(
+        #                 file_path, new_contents
+        #             ))))
 
-            except asyncio.TimeoutError:
-                continue
+
+        #     except asyncio.TimeoutError:
+        #         continue
