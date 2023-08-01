6436ae885ed895c7aabddd4ddba181e659b64118
jesse <jessemhan@proton.me>
2023-08-01 00:31:17 -0700

    restore load_documents

--- rift-engine/rift/server/lsp.py
+++ rift-engine/rift/server/lsp.py
@@ -1,8 +1,11 @@
+import glob
+import os
+import json
 import asyncio
 import logging
 import uuid
 from dataclasses import dataclass
-from typing import Any, List, Optional
+from typing import Any, List, Optional, Dict
 
 import rift.lsp.types as lsp
 from rift.agents import AGENT_REGISTRY, Agent, AgentRegistryResult, AgentParams
@@ -97,54 +100,55 @@ class LspServer(BaseLspServer):
         logger.info("workspace/didChangeConfiguration")
         await self.get_config()
 
-    # @rpc_method("morph/loadFiles")
-    # def load_documents(self, params: LoadFilesParams) -> LoadFilesResult:
-    #     try:
-    #         current_dir = os.path.abspath(__file__)
-    #     except:
-    #         current_dir = os.getcwd()
-    #     with open(os.path.join(current_dir, "languages.json"), "r") as f:
-    #         language_map = json.loads(f)
-    #
-    #     def find_matching_language(
-    #         filepath: str, language_map: Dict[str, List[Dict[str, str]]]
-    #     ) -> Optional[str]:
-    #         extension = filepath.split(".")[-1]  # Get the file extension
-    #
-    #         for details in language_map["languages"]:
-    #             if extension in details.get("extensions", []):
-    #                 return details["id"]
-    #
-    #         return None
-    #
-    #     def preprocess_filepaths(filepaths: List[str]) -> List[str]:
-    #         processed_filepaths = []
-    #         for filepath in filepaths:
-    #             processed_filepaths.append(os.path.expandvars(filepath))
-    #         return processed_filepaths
-    #
-    #     def join_filepaths(filepaths: List[str]) -> List[str]:
-    #         for filepath in filepaths:
-    #             yield from glob.glob(filepath, root="/" if filepath.startswith("/") else None)
-    #
-    #     for file_path in join_filepaths(preprocess_filepaths(params.patterns)):
-    #         with open(file_path, "r") as f:
-    #             text = f.read()
-    #         doc_item = lsp.TextDocumentItem(
-    #             text=text,
-    #             uri="file://" + os.path.join(os.getcwd(), str(file_path))
-    #             if not file_path.startswith("/")
-    #             else str(file_path),
-    #             languageId=find_matching_language(file_path, language_map) or "*",
-    #             version=1,
-    #         )
-    #         result_documents[doc_item.uri] = doc_item
-    #
-    #     result = LoadFilesResult(documents=result_documents)
-    #
-    #     self.documents.update(result.documents)
-    #
-    #     return result
+    @rpc_method("morph/loadFiles")
+    def load_documents(self, params: LoadFilesParams) -> LoadFilesResult:
+        try:
+            current_dir = os.path.abspath(__file__)
+        except:
+            current_dir = os.getcwd()
+        with open(os.path.join(current_dir, "languages.json"), "r") as f:
+            language_map = json.loads(f)
+    
+        def find_matching_language(
+            filepath: str, language_map: Dict[str, List[Dict[str, str]]]
+        ) -> Optional[str]:
+            extension = filepath.split(".")[-1]  # Get the file extension
+    
+            for details in language_map["languages"]:
+                if extension in details.get("extensions", []):
+                    return details["id"]
+    
+            return None
+    
+        def preprocess_filepaths(filepaths: List[str]) -> List[str]:
+            processed_filepaths = []
+            for filepath in filepaths:
+                processed_filepaths.append(os.path.expandvars(filepath))
+            return processed_filepaths
+    
+        def join_filepaths(filepaths: List[str]) -> List[str]:
+            for filepath in filepaths:
+                yield from glob.glob(filepath, root="/" if filepath.startswith("/") else None)
+
+        result_documents: Dict[str, lsp.TextDocumentItem]
+        for file_path in join_filepaths(preprocess_filepaths(params.patterns)):
+            with open(file_path, "r") as f:
+                text = f.read()
+            doc_item = lsp.TextDocumentItem(
+                text=text,
+                uri="file://" + os.path.join(os.getcwd(), str(file_path))
+                if not file_path.startswith("/")
+                else str(file_path),
+                languageId=find_matching_language(file_path, language_map) or "*",
+                version=1,
+            )
+            result_documents[doc_item.uri] = doc_item
+    
+        result = LoadFilesResult(documents=result_documents)
+    
+        self.documents.update(result.documents)
+    
+        return result
 
     @rpc_method("morph/applyWorkspaceEdit")
     async def on_workspace_did_change_configuration(self, params: lsp.ApplyWorkspaceEditParams):
