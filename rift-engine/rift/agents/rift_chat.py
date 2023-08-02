ded727319b9ab6e46afb3a2bc5aacb9594226929
jesse <jessemhan@proton.me>
2023-08-02 12:18:40 -0700

    add logging message

--- rift-engine/rift/agents/rift_chat.py
+++ rift-engine/rift/agents/rift_chat.py
@@ -108,6 +108,7 @@ class RiftChatAgent(Agent):
             )
             async for delta in stream.text:
                 response += delta
+                logger.info(f"{delta=}")
                 async with response_lock:
                     await self.send_progress(ChatProgress(response=response))
             await self.send_progress(ChatProgress(response=response, done_streaming=True))
