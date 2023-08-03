82dbb57d41447195569e2a96f86cb460174ee566
brentably <brentably@gmail.com>
2023-08-02 19:59:31 -0700

    delete useless comments

--- editors/rift-vscode/webviews/components/ChatWebview.svelte
+++ editors/rift-vscode/webviews/components/ChatWebview.svelte
@@ -8,14 +8,10 @@
 
 </script>
 
-<!-- This is the main ChatWebview component. It displays the Chat, AcceptRejectBar (if applicable), and OmniBar components. -->
 <div class="h-screen flex flex-col">
-  <!-- Display the Chat component. -->
   <Chat />
-  <!-- If the selected agent's state indicates that the AcceptRejectBar should be shown, display the AcceptRejectBar component. -->
   {#if $state.agents[$state.selectedAgentId]?.doesShowAcceptRejectBar}
     <AcceptRejectBar/>
   {/if}
-  <!-- Display the OmniBar component. -->
   <OmniBar />
 </div>
--- editors/rift-vscode/webviews/components/chat/dropdown/AtDropdown.svelte
+++ editors/rift-vscode/webviews/components/chat/dropdown/AtDropdown.svelte
@@ -1,27 +1,8 @@
 <script lang="ts">
-  import { state, filteredFiles, focusedFileIndex } from "../../stores"
-  import type { AtableFile } from "../../../../src/types"
+  import { filteredFiles, focusedFileIndex } from "../../stores"
   import AtDropdownCard from "./AtDropdownCard.svelte"
 
-  // this will be the latest @ to the end of the textbox, not including already made chips. if no matches show up then the user will be able to keep typing and the dropdown won't appear.
-  
-  
-
-  
-
-  // todo: make sure this is working on the individual card level.
-  // function handleKeyDown(e: KeyboardEvent) {
-  //   console.log("handleKeyDown in AtDropdown, ",$filteredFiles[$focusedFileIndex])
-  //   if (e.key === "Enter") {
-  //     e.preventDefault()
-  //     $filteredFiles[$focusedFileIndex].onEnter()
-  //   }
-  // }
-
-
 </script>
-
-<!-- <svelte:window on:keydown={handleKeyDown} /> -->
 
   <div class="absolute bottom-full left-0 px-2 w-full z-20 drop-shadow-[0_-4px_16px_0px_rgba(0,0,0,0.36)]">
     {#each $filteredFiles as file, index}
