33706cf23a3a218cf91678bc9217bfad97d211c7
jesse <jessemhan@proton.me>
2023-08-02 20:28:42 -0700

    Refactor _run_chat_thread to split the response stream into before and after parts and process them recursively.
    # Aider chat conversation:

--- rift-engine/rift/agents/response_stream_agent.py
+++ rift-engine/rift/agents/response_stream_agent.py
@@ -25,13 +25,35 @@ class ResponseStreamAgent(agent.Agent, ABC):
     An abstract class that implements a response_stream, a _response_buffer, and synchronous wrappers.
     """
 
-    @abstractmethod
-    async def _run_chat_thread(self, response_stream):
+    async def _run_chat_thread(self, response_stream: TextStream) -> None:
         """
-        Run the chat thread.
+        Run the chat thread. This function is defined as asynchronous to enable it to wait for and process each response
+        from the chat asynchronously. It is a recursive function that locks the async call, then processes the "before" part
+        of the message and calls itself again with the "after" part of the message.
+
         :param response_stream: The stream of responses from the chat.
+        :type response_stream: TextStream
         """
-        pass
+        # split the shared response stream into two parts around the first occurrence of the character "feeling"  
+        before, after = response_stream.split_once("feeling")
+
+        try:
+            # acquire an asyncio lock to prevent the response stream from being accessed
+            # by multiple threads simultaneously, protecting the integrity of the response data
+            async with self.state.response_lock:
+                # For each message 'delta' before the keyword, do the following processing
+                async for delta in before:
+                    # Append the 'delta' text to current '_response_buffer'
+                    self._response_buffer += delta
+                    # Send progress update after reading each line of the incoming stream
+                    await self.send_progress({"response": self._response_buffer})
+            # Some breathing space for the agent after processing the 'before' stream
+            await asyncio.sleep(0.1)
+            # call the function itself but now process the 'after' stream
+            await self._run_chat_thread(after)
+        except Exception as e:
+            # if exception arises, log and move forward. This prevents stalls on unforeseen exceptional cases
+            logger.info(f"[_run_chat_thread] caught exception={e}, exiting")
 
     @abstractmethod
     async def run(self) -> Any:
